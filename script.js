abf2f9a3589da9c722ff84f97c4fcd70e27f1c3b
Kaan Tan <kaantan97@gmail.com>
2024-04-14 09:48:49 +0300

    turn indicater has been introduced, and instead of wheel, this indicator will rotate

--- script.js
+++ script.js
@@ -18,24 +18,38 @@ canvas.width = 1000;
 boardCanvas.height = 606;
 boardCanvas.width = 1136;
 
+const turn_indicator = document.querySelector('.turn-indicator');
+let rotationAngle = 0;
+let counter = 0;
+turn_indicator.addEventListener('click', function() {
+    rotationAngle -= 60;
+    counter++;
+
+    if(counter % 3 == 0){
+        rotationAngle += 180;
+    }
+    this.style.transform = `rotate(${rotationAngle}deg)`;
+});
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
     var gridSize = { rows: 16, columns: 30 };
-    var boxSize = { width: 32, height: 32 };
+    var boxDimension = 32;
 
     let margin = 6;
 
     // Draw the boxes
     boxes.forEach(function (box) {
 
-        var _x = box.row *  (boxSize.height + margin);
-        var _y = box.column *  (boxSize.width + margin);
+        var _x = box.row *  (boxDimension + margin);
+        var _y = box.column *  (boxDimension + margin);
 
-        _mapper.set(box.color, { x: _x + (boxSize.width / 2), y: _y + (boxSize.height/2) });
+        _mapper.set(box.color, { x: _x + (boxDimension / 2), 
+                                 y: _y + (boxDimension / 2) });
 
         boardCanvasCtx.fillStyle = box.color;
-        boardCanvasCtx.fillRect(_x, _y, boxSize.width , boxSize.height );
+        boardCanvasCtx.fillRect(_x, _y, boxDimension , boxDimension );
     });
 
     // let imageData = boardCanvasCtx.getImageData(270, 485, 1, 1);
@@ -49,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 boardCanvas.addEventListener("click", function (e) {
 
-    let {x,y} =  getMousePos(boardCanvas,e.x,e.y)
+    let {x,y} =  localizeMousePos(boardCanvas,e.x,e.y)
     let _data = boardCanvasCtx.getImageData(x, y, 1, 1).data;
 
     let hexColor = rgbToHex(_data[0],_data[1],_data[2]);
@@ -79,13 +93,17 @@ function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-function getMousePos(canvas, mouseX,mouseY) {
+function localizeMousePos(canvas, mouseX,mouseY) {
     var rect = canvas.getBoundingClientRect();
     return {
         x: (mouseX - rect.left) / (rect.right - rect.left) * canvas.width,
         y: (mouseY - rect.top) / (rect.bottom - rect.top) * canvas.height
     };
 }
+
+function toggleRotation(element) {
+    element.classList.toggle('rotate');
+  }
 
 
 
