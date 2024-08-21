15721c923028587030229ea5023d1d331084d8fd
Kaan Tan <kaantan97@gmail.com>
2024-08-21 19:12:03 +0300

    Tiny refactorings are made

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 import boxes from "./models/boxes.js";
-
 const _mapper = new Map();
 
-const boardCanvas = document.getElementById("boardCanvas");
-const boardCanvasCtx = boardCanvas.getContext("2d",{ willReadFrequently: true });
 
-
-const canvas = document.getElementById("mainCanvas");
-const ctx = canvas.getContext("2d");
-
-
-
+const colorBoardCanvas = document.getElementById("colorBoardCanvas");
+const mainCanvas = document.getElementById("mainCanvas");
 const colorCardCanvas = document.getElementById("colorCardCanvas");
+
+const colorBoardCanvasCtx = colorBoardCanvas.getContext("2d", { willReadFrequently: true });
+const mainCanvasCtx = mainCanvas.getContext("2d");
 const colorCardCtx = colorCardCanvas.getContext("2d");
 
-canvas.height = 600;
-canvas.width = 1000;
 
-boardCanvas.height = 606;
-boardCanvas.width = 1136;
+console.log(`${mainCanvas.height} ${mainCanvas.width}`)
+console.log(`${colorBoardCanvas.height} ${colorBoardCanvas.width}`)
+mainCanvas.height = 600;
+mainCanvas.width = 1000;
+colorBoardCanvas.height = 606;
+colorBoardCanvas.width = 1136;
+console.log("------------------")
+console.log(`${mainCanvas.height} ${mainCanvas.width}`)
+console.log(`${colorBoardCanvas.height} ${colorBoardCanvas.width}`)
+
 
 
 const turn_indicator = document.querySelector('.turn-indicator');
 let rotationAngle = 0;
 let counter = 0;
-turn_indicator.addEventListener('click', function() {
+turn_indicator.addEventListener('click', function () {
     rotationAngle -= 60;
     counter++;
 
-    if(counter % 3 == 0){
+    if (counter % 3 == 0) {
         rotationAngle += 180;
     }
     this.style.transform = `rotate(${rotationAngle}deg)`;
@@ -41,56 +43,60 @@ document.addEventListener("DOMContentLoaded", function () {
     let margin = 6;
 
     // Draw the boxes
-    for (let curColumn=0; curColumn<boxes.length; curColumn++){
-        for(let curRow=0; curRow<boxes[curColumn].length; curRow++){
+    for (let curColumn = 0; curColumn < boxes.length; curColumn++) {
+        for (let curRow = 0; curRow < boxes[curColumn].length; curRow++) {
 
-            var _x = curColumn *  (boxDimension + margin);
-            var _y = curRow *  (boxDimension + margin);
-            
+            var _x = curColumn * (boxDimension + margin);
+            var _y = curRow * (boxDimension + margin);
+
             let box = boxes[curColumn][curRow];
-            _mapper.set(box.color, { x: _x + (boxDimension / 2), 
-                                     y: _y + (boxDimension / 2),
-                                     name:box.name });
-    
-            boardCanvasCtx.fillStyle = box.color;
-            boardCanvasCtx.fillRect(_x, _y, boxDimension , boxDimension );
+            _mapper.set(box.color, {
+                x: _x + (boxDimension / 2),
+                y: _y + (boxDimension / 2),
+                name: box.name
+            });
+
+            colorBoardCanvasCtx.fillStyle = box.color;
+            colorBoardCanvasCtx.fillRect(_x, _y, boxDimension, boxDimension);
         }
     }
 });
 
-boardCanvas.addEventListener("click", function (e) {
+colorBoardCanvas.addEventListener("click", function (e) {
 
-    let {x,y} =  localizeMousePos(boardCanvas,e.x,e.y)
-    let _data = boardCanvasCtx.getImageData(x, y, 1, 1).data;
+    let { x, y } = localizeMousePos(colorBoardCanvas, e.x, e.y)
+    let data = colorBoardCanvasCtx.getImageData(x, y, 1, 1).data;
 
-    let hexColor = rgbToHex(_data[0],_data[1],_data[2]);
+    let hexColor = rgbToHex(data[0], data[1], data[2]);
     let pos = _mapper.get(hexColor);
 
 
-    if(pos != undefined)
-    {
+    if (pos != undefined) {
         // movePlayerPawn();
         console.log(pos);
-    }else{
+    } else {
 
         console.log("basamadinKiii");
     }
-
-
 })
 
 
 
+
+
+
+
+
 function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-function localizeMousePos(canvas, mouseX,mouseY) {
+function localizeMousePos(canvas, mouseX, mouseY) {
     var rect = canvas.getBoundingClientRect();
     return {
         x: (mouseX - rect.left) / (rect.right - rect.left) * canvas.width,
@@ -100,7 +106,7 @@ function localizeMousePos(canvas, mouseX,mouseY) {
 
 function toggleRotation(element) {
     element.classList.toggle('rotate');
-  }
+}
 
 
 
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const app = express();
 const myServer = require('http').createServer(app);
 const ws = require('ws');
-
-const wss = new ws.Server({ server: myServer });
+const wsServer = new ws.Server({ server: myServer });
 
 const MAX_PLAYER_NUMBER = 4;
 let currentPlayerNumber = 0;
 
-wss.on("connection", function connection(webSocket) {
+wsServer.on("connection", function connection(webSocket) {
     if (currentPlayerNumber != MAX_PLAYER_NUMBER) {
         webSocket.terminate();
         return;
@@ -33,7 +30,7 @@ wss.on("connection", function connection(webSocket) {
             console.log(`User ${userId} performed action: ${action}`);
 
             // Broadcast the received message to all connected clients
-            wss.clients.forEach(function each(client) {
+            wsServer.clients.forEach(function each(client) {
                 if (client !== ws && client.readyState === WebSocket.OPEN) {
                     client.send(message);
                 }
@@ -47,11 +44,3 @@ wss.on("connection", function connection(webSocket) {
 
 });
 
-
-
-app.get("/",(req,res) => {
-    
-});
-
-const PORT = 5050;
-myServer.listen(PORT,() => console.log("Listening on port ${PORT}"))
